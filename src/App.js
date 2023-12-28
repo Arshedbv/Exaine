76222087cfc29612b026c86321d1623ab4a2a63e
Savvypool <125363786+Savvypool@users.noreply.github.com>
2023-12-28 20:00:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { ChatEngine } from 'react-chat-engine';
+import ExaineFeed from './components/ExaineFeed';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ChatEngine
+        height="100vh"
+        projectID="885c63d8-d693-4360-ae53-ad4d6918d749"
+        userName="arshedbv"
+        userSecret="123"
+        renderChatFeed={(exaineProps) => <ExaineFeed {...exaineProps}/>}
+      />
+    </>
   );
 }
 
---
+++ src/components/MessageForm.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <form className='chat-message-form' ></form>
+  )
+}
